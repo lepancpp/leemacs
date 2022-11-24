1cf0c5e807d61db5bbc107c6de1405997619e332
panle <panle@syriusrobotics.com>
2022-11-24 11:06:03 +0800

    first commit

---
+++ emacs.d/site-lisp/data/templates/template.mk
@@ -0,0 +1,8 @@
+#
+# Syrius Robotics Co., Ltd.  CONFIDENTIAL
+#
+# Unpublished Copyright (c) 2022 [Syrius Robotics Co., Ltd.],
+# All Rights Reserved.
+#
+#    Authors: Le Pan <panle@syriusrobotics.com>
+#
