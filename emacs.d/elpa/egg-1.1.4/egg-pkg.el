137071d320d88f93ba1069516b27298a58a46935
panle <panle@syriusrobotics.com>
2025-04-14 13:44:08 +0800

    feat: Update new externsion for emacs29.03

---
+++ emacs.d/elpa/egg-1.1.4/egg-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "egg" "1.1.4"
+  "Emacs Got Git - Emacs interface to Git."
+  ()
+  :url "https://github.com/byplayer/egg"
+  :commit "00e768a78ac3d25f457eed667d02cac568480bf9"
+  :revdesc "00e768a78ac3"
+  :keywords '("git" "version control" "release management")
+  :authors '(("Bogolisk" . "bogolisk@gmail.com"))
+  :maintainers '(("Bogolisk" . "bogolisk@gmail.com")))
